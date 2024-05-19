9d60d604a2b160abec246b2373cffdb4ec598851
Namkha Gyatso <namkhagyatso999@gmail.com>
2024-05-19 17:28:30 +0000

    data-types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,12 @@
+"use strict"; // treall all js code as newer version
+
+// alert(3+3) we are using node js
+
+let name = "NAMKHA"
+let age = 18
+let flag = true
+let state = null
+
+//symbol is used for uniqueness
+console.log(typeof name)
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 14455 //constant
+let accountEmail = "namkha@gmail.com" //
+// var accountPassword = "namkha" var is not used nowadays because of issue in scope
+accountCity = "Kathmandu" //not good practise
+let accountState; //it takes it as undefined
+
+// accountId = 2
+accountEmail = "gyatso999@gmail.com"
+accountPassword = "221133"
+accountCity = "KTM"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
