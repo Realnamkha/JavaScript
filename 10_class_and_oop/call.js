1f9dd0629ef5ed0a799a95bc478967eb1b5121b0
Namkha Gyatso <namkhagyatso999@gmail.com>
2024-06-15 05:26:22 +0000

    call and this

---
+++ 10_class_and_oop/call.js
@@ -0,0 +1,16 @@
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    // SetUsername(username) // The function is being called but as soon as it is called it is removed from exexution context
+    SetUsername.call(this, username) // do this in mine
+   
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai","chai@gmail.com","123")
+console.log(chai)
