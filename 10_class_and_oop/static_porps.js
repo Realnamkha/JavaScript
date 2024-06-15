7febbc4f7c7148311f07d209baf69afc3412aeac
Namkha Gyatso <namkhagyatso999@gmail.com>
2024-06-15 05:43:33 +0000

    inheritance and static

---
+++ 10_class_and_oop/static_porps.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId()) it is not accessible
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+// console.log(iphone.createId()); this is also not accessible due to static keyword
