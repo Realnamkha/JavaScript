4f0186dda8a18fbd7c09617d6324a154efcfc78e
Namkha Gyatso <namkhagyatso999@gmail.com>
2024-12-24 16:01:02 +0000

    "comments updated"

--- 01_basics/data_conversion.js
+++ 01_basics/data_conversion.js
@@ -3,7 +3,13 @@ let score = "33"
 //console.log(typeof score)
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber)
+// The Number() function attempts to convert its argument into a numeric value.
+//If the argument cannot be converted to a valid number (e.g., non-numeric strings like "hitesh"), 
+// the result is NaN (Not-a-Number).
+//console.log(typeof valueInNumber) //number
+// typeof score;             // "string"
+// typeof valueInNumber;     // "number"
+// valueInNumber;            // NaN
 
 // "33" - 33
 // "33abc" - NAN
@@ -11,7 +17,10 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn)
+//The Boolean() function converts a value into its corresponding boolean 
+// representation (true or false) based on JavaScript's concept of "truthy" 
+// and "falsy" values.
+//console.log(booleanIsLoggedIn) is True because it is truthy value
 
 // OPEARATIONS
 // console.log(2+2)
@@ -26,6 +35,11 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log("1" + "2")
 // console.log("1" + 2 + 2)
 // console.log(1 + 2 + "2")
+// When one operand is a string and the other is a number, JavaScript 
+// converts the number to a string and performs string concatenation.
+// The + operator is left-associative, meaning it evaluates from left to 
+// right in expressions with multiple operators. This is why "1" + 2 + 2 results 
+// in "122", while 1 + 2 + "2" results in "32".
 
 // console.log(+true)
 // console.log(+"")
